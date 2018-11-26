e44fd3a4dc833e75ca2879ba312a9b93d20ddde5
Abhay Bhargav <abhay@we45.com>
2018-11-26 10:53:49 +0530

    changed secret

--- kms/secrets_manager/secrets_example.py
+++ kms/secrets_manager/secrets_example.py
@@ -4,7 +4,7 @@ secret = boto3.client('secretsmanager')
 
 # create secret
 new_secret = secret.create_secret(
-    Name = "jwt_hmac_token",
+    Name = "jwt_hmac_token_2",
     Description = "This is the Token to generate and verify JWT tokens for the API",
     SecretString = "8a39b257-98d7-4448-822c-6fa131f57240"
 )
