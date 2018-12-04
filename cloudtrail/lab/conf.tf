5d9b98466e0cc3c18d43b6187c6f50934d89e10a
Tilak T <tilak.thimmappa@we45.com>
2018-12-04 12:12:01 +0530

    cloudtrail
    cloudtrail log all s3 bucket events example

--- cloudtrail/lab/conf.tf
+++ cloudtrail/lab/conf.tf
@@ -1,16 +1,21 @@
-provider "aws" {
-  region     = "us-east-1"
-}
-
-resource "aws_cloudtrail" "cloudtrial" {
-  name                          = "tf-trail-cloudtrial"
-  s3_bucket_name                = "${aws_s3_bucket.s3-bucket.id}"
+resource "aws_cloudtrail" "we45_cloudtrial" {
+  name                          = "we45-trail-cloudtrial"
+  s3_bucket_name                = "${aws_s3_bucket.we45-s3-bucket.id}"
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
+  event_selector {
+    read_write_type = "All"
+    include_management_events = true
+
+    data_resource {
+      type = "AWS::S3::Object"
+      values = ["arn:aws:s3:::"]
+    }
+  }
 }
 
-resource "aws_s3_bucket" "s3-bucket" {
-  bucket        = "tf-test-trail"
+resource "aws_s3_bucket" "we45-s3-bucket" {
+  bucket        = "we45-cloud-trail"
   force_destroy = true
 
   policy = <<POLICY
